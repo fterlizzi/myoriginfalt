cab6222ee6074d2c449fd9042937785d3a2deab1
fterlizzi <fterlizzi@gmail.com>
2022-01-01 09:32:56 +0100

    Initial Commit

---
+++ MySecondPrj_confGitHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySecondPrj_confGitHub
+//
+//  Created by Francesco terlizzi on 01/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
